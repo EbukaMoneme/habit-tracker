f236a3dc5ea39708b982fc57ab0fc10b6fd7ac63
Ebuka Moneme <camoneme@gmail.com>
2022-01-03 01:10:16 -0500

    Create Daily Habit add state and 'complete function'

--- components/DailyHabit.js
+++ components/DailyHabit.js
@@ -1,7 +1,37 @@
+import { useState } from 'react'
+import styles from '../styles/DailyHabit.module.css'
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+
 export default function DailyHabit(props) {
+	// console.log(props)
+	const [isCompleted, setIsCompleted] = useState(false)
 	return (
-		<div>
-			
+		<div 
+			className={styles.habit} 
+			style={
+				isCompleted?
+				{backgroundColor: 'white', color: 'black', borderLeft: `6px solid ${props.color}`, borderRadius: '0px 7px 7px 0px'}
+				:
+				{backgroundColor: props.color, color: 'white'}
+			}
+		>
+			<div className={styles.title}>{props.title}</div>
+			{isCompleted?
+				<button 
+					className={styles.completebutton}
+					onClick={() => setIsCompleted(!isCompleted)}
+				>Mark Complete</button>
+				:
+				<div className={styles.habitfooter}>
+					<div >
+						<FontAwesomeIcon icon={faCheck}></FontAwesomeIcon> {' '}
+						Completed
+					</div>
+					<button onClick={() => setIsCompleted(!isCompleted)}>Undo</button>
+				</div>
+			}
 		</div>
 	)
 }
