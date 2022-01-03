b3350881b599e3db542eeccae2c622f7fb9a28b6
Ebuka Moneme <camoneme@gmail.com>
2022-01-03 01:07:38 -0500

    Create daily habit container, parse habits

--- components/DailyContainer.js
+++ components/DailyContainer.js
@@ -1,9 +1,15 @@
 import styles from "../styles/DailyContainer.module.css"
+import DailyHabit from "./DailyHabit"
 
 export default function DailyContainer(props) {
+
+	const parsedHabits = props.habits && props.habits.map((habit, index) => <DailyHabit {...habit} key={index}/>)
 	return (
 		<div className={styles.container}>
-			Daily Container
+			Sat, Jan 1
+			<div className={styles.dailyhabits}>
+				{parsedHabits}
+			</div>
 		</div>
 	)
 }
